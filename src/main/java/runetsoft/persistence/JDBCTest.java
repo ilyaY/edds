f5ebdfd7317fe1ba30ec1b64410e975adc59beef
Ilya Yatsishin <ilya@delightex.com>
2017-11-27 11:57:31 +0100

    jdbc test

--- src/main/java/runetsoft/persistence/JDBCTest.java
+++ src/main/java/runetsoft/persistence/JDBCTest.java
@@ -7,13 +7,19 @@ import java.sql.Statement;
 
 public class JDBCTest {
   public static void main(String[] args) throws Exception {
+    if (args == null || args.length != 3) {
+      System.out.println("Usage : JDBCTest [db_url] [db_username] [db_password]");
+      System.exit(0);
+    }
+
     Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/edds", "root", "ro123ot");
+    Connection conn = DriverManager.getConnection(args[0], args[1], args[2]);
     Statement stmt = conn.createStatement();
     ResultSet rs = stmt.executeQuery("SELECT * FROM CARS");
     while (rs.next()) {
       System.out.println(rs.getInt("ID"));
       System.out.println(rs.getString("NAME"));
+      System.out.println(rs.getString("CAR_KEY"));
     }
     conn.close();
   }
